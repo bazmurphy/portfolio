fb8234066ad8b3850a57b8d25e3b008783cce251
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-08-04 22:12:52 +0100

    header light/dark mode start

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,32 @@
 import "./Header.css";
+import { LuSun, LuMoon } from "react-icons/lu";
+import { useState } from "react";
 
 const Header = () => {
+  // refactor to context & provider etc.
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
     <header>
       <h3>Header</h3>
+      <div className="mode-container">
+        {isDarkMode && (
+          <button
+            className="mode-button"
+            onClick={() => setIsDarkMode(!isDarkMode)}
+          >
+            <LuSun className="mode-icon mode-light-icon" />
+          </button>
+        )}
+        {!isDarkMode && (
+          <button
+            className="mode-button"
+            onClick={() => setIsDarkMode(!isDarkMode)}
+          >
+            <LuMoon className="mode-icon mode-dark-icon" />
+          </button>
+        )}
+      </div>
     </header>
   );
 };
